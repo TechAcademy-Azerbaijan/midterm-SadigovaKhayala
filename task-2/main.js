d858fbafb7cab239511e79aa5dcd735683d86b6b
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-21 20:46:51 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -15,22 +15,28 @@ prompt.get('input', function (err, result) {
     var n = parseInt(numbers[0]); // Ilk daxil olunmuş ədədi alırıq. Nümunə - 12 
     let num=0;
     let c=n%10;
-    let b=parseInt(n/10)%10;
-    let a=parseInt(n/100)
-    if(a>b&&a>c){
-       num= a*100+b*10+c
-    }
-    else if(a<b&&b>c&&c<a){
-        num= b*100+a*10+c
-    }
-    else if (a<c&&b<c){
-        num= c*100+b*10+a
-    }
-    else if(b>a&&b>c){
-        num=b*100+c*10+a
-    }
-    
-    
-    console.log(num*num)
+let b=parseInt(n/10)%10;
+let a=parseInt(n/100)
+if(a>b&&a>c&&b>c){
+   num= a*100+b*10+c
+}
+else if(a<b&&a<c&&b>c){
+    num= b*100+c*10+a
+}
+else if (a<c&&b<c){
+    num= c*100+b*10+a
+}
+else if(b>a&&b>c&&a>c){
+    num=b*100+c*10+a
+}
+else if(b>a&&b>c&&a<c){
+    num=b*100+c*10+a
+}
+else if(b>a&&b>c&&a<c){
+    num=b*100+c*10+a
+}
+
+
+console.log(num**2)
     
 });
