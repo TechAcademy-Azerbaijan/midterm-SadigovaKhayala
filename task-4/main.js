284a1e9afe82940257f075fbe89414c430299a5b
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-21 21:01:27 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -11,13 +11,14 @@ prompt.get('input', function (err, result) {
     var input = result.input; // "12,5"
     var numbers = input.split(','); // ['12', '5']
     var a = parseInt(numbers[0]); // Ilk daxil olunmuş ədədi alırıq. Nümunə - 12 
-    let b;
-    let i=a-1;
-    for(i<a;i>1;i--){
-        if(i%2!=0&&a%i==0){
-    
-            console.log(i)
-        }
+  
+
+for(i=1;i<=a;i++){
+    if(i%2!=0&&a%i==0){
+
+        console.log(i)
     }
+}
+
     
 });
